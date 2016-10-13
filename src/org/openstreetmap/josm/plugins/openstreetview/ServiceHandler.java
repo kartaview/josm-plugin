02c0bf50241cc9e9a25f127d051e3c7b640ae263
beataj <beataj@172.22.22.120>
2016-10-13 14:36:11 +0300

    - added support for OsmDataLayer

--- src/org/openstreetmap/josm/plugins/openstreetview/ServiceHandler.java
+++ src/org/openstreetmap/josm/plugins/openstreetview/ServiceHandler.java
@@ -17,7 +17,14 @@ package org.openstreetmap.josm.plugins.openstreetview;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import javax.swing.JOptionPane;
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.gui.JosmUserIdentityManager;
@@ -48,30 +55,59 @@ final class ServiceHandler {
         service = new OpenStreetViewService();
     }
 
+    /**
+     * 
+     * @return
+     */
     static ServiceHandler getInstance() {
         return INSTANCE;
     }
 
+
     /**
-     * List the photos from the current area based on the given filters.
-     *
-     * @param circle a {@code Circle} represents the search area
+     * Lists the photos from the current area based on the given filters.
+     * 
+     * @param areas a list of {@code Circle}s representing the search areas. If the OsmDataLayer is active, there might
+     * be several bounds.
      * @param filter a {@code Filter} represents the user's search filters. Null values are ignored
      * @return a list of {@code Photo}s
      */
-    List<Photo> listNearbyPhotos(final Circle circle, final ListFilter filter) {
-        List<Photo> result = new ArrayList<>();
-        final Long userId = filter != null && filter.isOnlyUserFlag()
+    public List<Photo> listNearbyPhotos(final List<Circle> areas, final ListFilter filter) {
+        final Long osmUserId = filter != null && filter.isOnlyUserFlag()
                 ? JosmUserIdentityManager.getInstance().asUser().getId() : null;
         final Date date = filter != null ? filter.getDate() : null;
+        List<Photo> finalResult = new ArrayList<>();
         try {
-            result = service.listNearbyPhotos(circle, date, userId, Paging.DEFAULT);
+            if (areas.size() > 1) {
+                Set<Photo> result = new HashSet<>();
+                final ExecutorService executor = Executors.newFixedThreadPool(areas.size());
+                final List<Future<List<Photo>>> futures = new ArrayList<>();
+                for (Circle circle : areas) {
+                    futures.add(executor.submit(new Callable<List<Photo>>() {
+                        @Override
+                        public List<Photo> call() throws OpenStreetViewServiceException {
+                            return service.listNearbyPhotos(circle, date, osmUserId, Paging.DEFAULT);
+                        }
+                    }));
+                }
+                for (Future<List<Photo>> future : futures) {
+                    try {
+                        result.addAll(future.get());
+                    } catch (InterruptedException | ExecutionException e) {
+                        throw new OpenStreetViewServiceException(e);
+                    }
+                }
+                finalResult.addAll(result);
+                executor.shutdown();
+            } else {
+                finalResult = service.listNearbyPhotos(areas.get(0), date, osmUserId, Paging.DEFAULT);
+            }
         } catch (final OpenStreetViewServiceException e) {
             if (!PreferenceManager.getInstance().loadErrorSuppressFlag()) {
                 JOptionPane.showMessageDialog(Main.parent, e.getMessage(),
                         GuiConfig.getInstance().getErrorPhotoListTxt(), JOptionPane.ERROR_MESSAGE);
             }
         }
-        return result;
+        return finalResult;
     }
 }
