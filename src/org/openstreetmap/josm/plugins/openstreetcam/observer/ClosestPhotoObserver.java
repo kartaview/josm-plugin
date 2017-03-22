3d8f6fb9b6aafbd9575bb4b8c3ef986ffc9a955e
ioanao-telenav <ioana.opric@telenav.com>
2017-03-22 08:55:36 +0200

    FCDV-3847 Update start template

--- src/org/openstreetmap/josm/plugins/openstreetcam/observer/ClosestPhotoObserver.java
+++ src/org/openstreetmap/josm/plugins/openstreetcam/observer/ClosestPhotoObserver.java
@@ -1,10 +1,10 @@
 /*
- * Copyright ©2017, Telenav, Inc. All Rights Reserved
- *
  * The code is licensed under the LGPL Version 3 license http://www.gnu.org/licenses/lgpl-3.0.en.html.
- * The collected imagery is protected & available under the CC BY-SA version 4 International license.
- * https://creativecommons.org/licenses/by-sa/4.0/ *legalcode.
  *
+ * The collected imagery is protected & available under the CC BY-SA version 4 International license.
+ * https://creativecommons.org/licenses/by-sa/4.0/legalcode.
+ *
+ * Copyright ©2017, Telenav, Inc. All Rights Reserved
  */
 package org.openstreetmap.josm.plugins.openstreetcam.observer;
 
