58dfa52bcb8098f8e32be78eb2efa88bf69d454e
beataj <beataj@10.230.7.20>
2017-07-03 14:01:55 +0300

    updated license text

--- src/org/openstreetmap/josm/plugins/openstreetcam/gui/details/Formatter.java
+++ src/org/openstreetmap/josm/plugins/openstreetcam/gui/details/Formatter.java
@@ -1,17 +1,10 @@
 /*
- *  Copyright 2016 Telenav, Inc.
+ * The code is licensed under the LGPL Version 3 license http://www.gnu.org/licenses/lgpl-3.0.en.html.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ * The collected imagery is protected & available under the CC BY-SA version 4 International license.
+ * https://creativecommons.org/licenses/by-sa/4.0/legalcode.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Copyright ©2017, Telenav, Inc. All Rights Reserved
  */
 package org.openstreetmap.josm.plugins.openstreetcam.gui.details;
 
