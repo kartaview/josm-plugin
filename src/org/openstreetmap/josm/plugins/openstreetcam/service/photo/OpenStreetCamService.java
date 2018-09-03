32e60a9887b0711fcf8637acd18157ed2c866a8c
beataj <beataj@10.230.6.42>
2018-09-03 15:52:45 +0300

    removed sysouts

--- src/org/openstreetmap/josm/plugins/openstreetcam/service/photo/OpenStreetCamService.java
+++ src/org/openstreetmap/josm/plugins/openstreetcam/service/photo/OpenStreetCamService.java
@@ -116,7 +116,6 @@ public class OpenStreetCamService extends BaseService {
      */
     public byte[] retrievePhoto(final String photoName) throws ServiceException {
         final String url = OpenStreetCamServiceConfig.getInstance().getServiceBaseUrl().concat(photoName);
-        System.out.println(url);
         byte[] image;
         try {
             final HttpConnector connector = new HttpConnector(url, getHeaders());
