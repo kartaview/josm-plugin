9556072281c19fbf9ba317ceca2da7de006af7d5
beataj <beataj@10.230.6.42>
2018-10-29 14:04:37 +0200

    corrected drawClusters method

--- src/org/openstreetmap/josm/plugins/openstreetcam/gui/layer/PaintHandler.java
+++ src/org/openstreetmap/josm/plugins/openstreetcam/gui/layer/PaintHandler.java
@@ -132,22 +132,22 @@ class PaintHandler {
             final boolean drawPhotos) {
         final Double arrowLength =
                 Util.zoom(mapView.getRealBounds()) > MIN_ARROW_ZOOM ? ARROW_LENGTH * mapView.getScale() : null;
-        graphics.setColor(PaintUtil.lineColor(mapView, Constants.SEQUENCE_LINE_COLOR));
+                graphics.setColor(PaintUtil.lineColor(mapView, Constants.SEQUENCE_LINE_COLOR));
 
-        Photo prevPhoto = photos.get(0);
-        for (int i = 1; i <= photos.size() - 1; i++) {
-            final Photo currentPhoto = photos.get(i);
-            // at least one of the photos is in current view draw line
-            drawLine(graphics, mapView, prevPhoto.getPoint(), currentPhoto.getPoint(), arrowLength);
+                Photo prevPhoto = photos.get(0);
+                for (int i = 1; i <= photos.size() - 1; i++) {
+                    final Photo currentPhoto = photos.get(i);
+                    // at least one of the photos is in current view draw line
+                    drawLine(graphics, mapView, prevPhoto.getPoint(), currentPhoto.getPoint(), arrowLength);
 
-            if (drawPhotos) {
-                drawPhoto(graphics, mapView, prevPhoto, false);
-            }
-            prevPhoto = currentPhoto;
-        }
-        if (drawPhotos) {
-            drawPhoto(graphics, mapView, prevPhoto, false);
-        }
+                    if (drawPhotos) {
+                        drawPhoto(graphics, mapView, prevPhoto, false);
+                    }
+                    prevPhoto = currentPhoto;
+                }
+                if (drawPhotos) {
+                    drawPhoto(graphics, mapView, prevPhoto, false);
+                }
     }
 
     void drawDetections(final Graphics2D graphics, final MapView mapView, final List<Detection> detections,
@@ -184,7 +184,7 @@ class PaintHandler {
                 graphics.setComposite(OPAQUE_COMPOSITE);
                 drawPhoto(graphics, mapView, selectedPhoto, true);
             } else {
-                if (selectedPhoto != null && !selectedPhoto.getPoint().equals(selectedPhoto.getPoint())) {
+                if (selectedPhoto != null && !selectedCluster.getPoint().equals(selectedPhoto.getPoint())) {
                     drawPhoto(graphics, mapView, selectedPhoto, true);
                 }
                 if (selectedDetection != null && !selectedCluster.getPoint().equals(selectedDetection.getPoint())) {
@@ -268,9 +268,9 @@ class PaintHandler {
         for (final Photo photo : cluster.getPhotos()) {
             final List<Detection> photoDetections =
                     cluster.getDetections().stream()
-                            .filter(d -> d.getSequenceId().equals(photo.getSequenceId())
-                                    && d.getSequenceIndex().equals(photo.getSequenceIndex()))
-                            .collect(Collectors.toList());
+                    .filter(d -> d.getSequenceId().equals(photo.getSequenceId())
+                            && d.getSequenceIndex().equals(photo.getSequenceIndex()))
+                    .collect(Collectors.toList());
             metadata.put(photo, photoDetections);
         }
         graphics.setColor(PaintUtil.lineColor(mapView, Constants.CLUSTER_DATA_LINE_COLOR));
