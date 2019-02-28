0296a9be2246249fbe649d6081dbf4fd82efe12a
laurad <laura.dumitru@telenav.com>
2019-02-28 16:42:29 +0200

    APO-6202 Added copyright
    - added the copyright licence text to the new classes

--- src/org/openstreetmap/josm/plugins/openstreetcam/entity/DownloadedWay.java
+++ src/org/openstreetmap/josm/plugins/openstreetcam/entity/DownloadedWay.java
@@ -1,3 +1,12 @@
+/*
+ * The code is licensed under the LGPL Version 3 license http://www.gnu.org/licenses/lgpl-3.0.en.html.
+ *
+ * The collected imagery is protected & available under the CC BY-SA version 4 International license.
+ * https://creativecommons.org/licenses/by-sa/4.0/legalcode.
+ *
+ * Copyright (c)2019, Telenav, Inc. All Rights Reserved
+ */
+
 package org.openstreetmap.josm.plugins.openstreetcam.entity;
 
 import org.openstreetmap.josm.data.coor.LatLon;
