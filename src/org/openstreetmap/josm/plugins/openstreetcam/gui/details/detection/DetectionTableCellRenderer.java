4bce08b31dc897607bbc8776f6a15c89735e32b5
nicoletav <nicoletav@telenav.com>
2019-04-03 14:01:20 +0300

    improved trackId column in detections table

--- src/org/openstreetmap/josm/plugins/openstreetcam/gui/details/detection/DetectionTableCellRenderer.java
+++ src/org/openstreetmap/josm/plugins/openstreetcam/gui/details/detection/DetectionTableCellRenderer.java
@@ -10,6 +10,8 @@ package org.openstreetmap.josm.plugins.openstreetcam.gui.details.detection;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
+import java.util.ArrayList;
+import java.util.Arrays;
 import javax.swing.JTable;
 import javax.swing.border.MatteBorder;
 import javax.swing.table.DefaultTableCellRenderer;
@@ -21,6 +23,7 @@ import org.openstreetmap.josm.plugins.openstreetcam.entity.ValidationStatus;
 import com.telenav.josm.common.formatter.DateFormatter;
 import com.telenav.josm.common.formatter.DecimalPattern;
 import com.telenav.josm.common.formatter.EntityFormatter;
+import org.openstreetmap.josm.plugins.openstreetcam.util.cnf.GuiConfig;
 
 
 /**
@@ -34,24 +37,27 @@ class DetectionTableCellRenderer extends DefaultTableCellRenderer {
     private static final String ZERO = "0";
     private static final String NULL_CONFIDENCE_LEVEL_TEXT = "-/-/-/-/-/-";
     private static final String DELIMITER = "/";
+    private static final int IDX_ID = 0;
     private static final int IDX_TRACKING_ID = 2;
     private static final double APPROXIMATED_ZERO_DOUBLE = 0.0;
     private static final float APPROXIMATED_ZERO_FLOAT = 0.0f;
     private static final Color HEADER_GRAY = new Color(235, 237, 239);
-    private static final long MIN_DATE_VALUE = 1000000000;
 
     public Component getTableCellRendererComponent(final JTable table, final Object value, final boolean isSelected,
             final boolean hasFocus, final int row, final int column) {
         super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
         setFont(MainApplication.getMap().getFont().deriveFont(Font.PLAIN));
+        String txt = "-";
         if (value != null) {
-            String txt = "-";
-            if (value instanceof Long) {
-                if ((long) value >= MIN_DATE_VALUE && column != IDX_TRACKING_ID) {
-                    txt = DateFormatter.formatTimestamp((Long) value);
-                } else {
-                    txt = value.toString();
-                }
+            ArrayList headerList = new ArrayList<>(Arrays.asList(GuiConfig.getInstance().getClusterTableHeader()));
+            if (value instanceof String && headerList.contains(value)) {
+                txt = value.toString();
+                setBackground(HEADER_GRAY);
+                setBorder(new MatteBorder(0, 0, 1, 1, Color.gray));
+                setFont(MainApplication.getMap().getFont().deriveFont(Font.BOLD));
+            } else if (value instanceof ValidationStatus || value instanceof EditStatus || column == IDX_TRACKING_ID ||
+                    column == IDX_ID) {
+                txt = value.toString();
             } else if (value instanceof ConfidenceLevel) {
                 if (((ConfidenceLevel) value).isNotNull()) {
                     ConfidenceLevel confidenceLevel = (ConfidenceLevel) value;
@@ -64,8 +70,8 @@ class DetectionTableCellRenderer extends DefaultTableCellRenderer {
                 } else {
                     txt = NULL_CONFIDENCE_LEVEL_TEXT;
                 }
-            } else if (value instanceof ValidationStatus || value instanceof EditStatus) {
-                txt = value.toString();
+            } else if (value instanceof Long) {
+                txt = DateFormatter.formatTimestamp((Long) value);
             } else if (value instanceof Double) {
                 if ((double) value != APPROXIMATED_ZERO_DOUBLE) {
                     txt = EntityFormatter.formatDouble((double) value, false, DecimalPattern.SHORT);
@@ -79,15 +85,9 @@ class DetectionTableCellRenderer extends DefaultTableCellRenderer {
                 } else {
                     txt = ZERO;
                 }
-            } else if (value instanceof String) {
-                txt = value.toString();
-                setBackground(HEADER_GRAY);
-                setBorder(new MatteBorder(0, 0, 1, 1, Color.gray));
-                setFont(MainApplication.getMap().getFont().deriveFont(Font.BOLD));
             }
-
-            setText(txt);
         }
+        setText(txt);
         return this;
     }
 
