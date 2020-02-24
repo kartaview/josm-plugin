473ebcd60a0cc0c8c856e509416138e929c52d6b
Beata Tautan-Jancso <beata.tautan@grabtaxi.com>
2020-02-24 12:04:35 +0200

    added deserialzer for confidence level

--- src/org/openstreetmap/josm/plugins/openstreetcam/service/apollo/ApolloService.java
+++ src/org/openstreetmap/josm/plugins/openstreetcam/service/apollo/ApolloService.java
@@ -11,6 +11,7 @@ import java.util.Date;
 import java.util.List;
 import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.plugins.openstreetcam.entity.Cluster;
+import org.openstreetmap.josm.plugins.openstreetcam.entity.ClusterConfidenceLevel;
 import org.openstreetmap.josm.plugins.openstreetcam.entity.Contribution;
 import org.openstreetmap.josm.plugins.openstreetcam.entity.Detection;
 import org.openstreetmap.josm.plugins.openstreetcam.entity.EditStatus;
@@ -24,7 +25,6 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.grab.josm.common.argument.BoundingBox;
 
-
 /**
  * Executes the operations of the ApolloService components.
  *
@@ -33,104 +33,104 @@ import com.grab.josm.common.argument.BoundingBox;
  */
 public class ApolloService extends BaseService {
 
-    private static final double AREA_EXTEND = 0.004;
+	private static final double AREA_EXTEND = 0.004;
 
-    @Override
-    public Gson createGson() {
-        final GsonBuilder builder = new GsonBuilder();
-        builder.registerTypeAdapter(EditStatus.class, new EditStatusTypeAdapter());
-        builder.registerTypeAdapter(LatLon.class, new LatLonDeserializer());
-        return builder.create();
-    }
+	@Override
+	public Gson createGson() {
+		final GsonBuilder builder = new GsonBuilder();
+		builder.registerTypeAdapter(EditStatus.class, new EditStatusTypeAdapter());
+		builder.registerTypeAdapter(LatLon.class, new LatLonDeserializer());
+		builder.registerTypeAdapter(ClusterConfidenceLevel.class, new ClusterConfidenceLevelDeserializer());
+		return builder.create();
+	}
 
+	public List<Detection> searchDetections(final BoundingBox area, final Date date, final Long osmUserId,
+			final DetectionFilter detectionFilter) throws ServiceException {
+		final String url = new HttpQueryBuilder().buildSearchDetectionsQuery(area, date, osmUserId, detectionFilter);
+		final Response response = executeGet(url, Response.class);
+		verifyResponseStatus(response);
+		return response.getDetections() != null ? response.getDetections() : new ArrayList<>();
+	}
 
-    public List<Detection> searchDetections(final BoundingBox area, final Date date, final Long osmUserId,
-            final DetectionFilter detectionFilter) throws ServiceException {
-        final String url = new HttpQueryBuilder().buildSearchDetectionsQuery(area, date, osmUserId, detectionFilter);
-        final Response response = executeGet(url, Response.class);
-        verifyResponseStatus(response);
-        return response.getDetections() != null ? response.getDetections() : new ArrayList<>();
-    }
+	public List<Cluster> searchClusters(final BoundingBox area, final Date date, final DetectionFilter detectionFilter)
+			throws ServiceException {
+		final BoundingBox extendedArea = new BoundingBox(area.getNorth() + AREA_EXTEND, area.getSouth() - AREA_EXTEND,
+				area.getEast() + AREA_EXTEND, area.getWest() - AREA_EXTEND);
+		final String url = new HttpQueryBuilder().buildSearchClustersQuery(extendedArea, date, detectionFilter);
+		final Response response = executeGet(url, Response.class);
+		verifyResponseStatus(response);
+		return response.getClusters() != null ? response.getClusters() : new ArrayList<>();
+	}
 
-    public List<Cluster> searchClusters(final BoundingBox area, final Date date, final DetectionFilter detectionFilter)
-            throws ServiceException {
-        final BoundingBox extendedArea = new BoundingBox(area.getNorth() + AREA_EXTEND, area.getSouth() - AREA_EXTEND,
-                area.getEast() + AREA_EXTEND, area.getWest() - AREA_EXTEND);
-        final String url = new HttpQueryBuilder().buildSearchClustersQuery(extendedArea, date, detectionFilter);
-        final Response response = executeGet(url, Response.class);
-        verifyResponseStatus(response);
-        return response.getClusters() != null ? response.getClusters() : new ArrayList<>();
-    }
+	public void updateDetection(final Detection detection, final Contribution contribution) throws ServiceException {
+		final String url = new HttpQueryBuilder().buildCommentQuery();
+		final Request request = new Request(detection, contribution);
+		final String content = buildRequest(request, Request.class);
+		final Response root = executePost(url, content, Response.class);
+		verifyResponseStatus(root);
+	}
 
-    public void updateDetection(final Detection detection, final Contribution contribution) throws ServiceException {
-        final String url = new HttpQueryBuilder().buildCommentQuery();
-        final Request request = new Request(detection, contribution);
-        final String content = buildRequest(request, Request.class);
-        final Response root = executePost(url, content, Response.class);
-        verifyResponseStatus(root);
-    }
+	public List<Detection> retrieveSequenceDetections(final Long sequenceId) throws ServiceException {
+		final String url = new HttpQueryBuilder().buildRetrieveSequenceDetectionsQuery(sequenceId);
+		final Response response = executeGet(url, Response.class);
+		verifyResponseStatus(response);
+		return response.getDetections();
+	}
 
-    public List<Detection> retrieveSequenceDetections(final Long sequenceId) throws ServiceException {
-        final String url = new HttpQueryBuilder().buildRetrieveSequenceDetectionsQuery(sequenceId);
-        final Response response = executeGet(url, Response.class);
-        verifyResponseStatus(response);
-        return response.getDetections();
-    }
+	public List<Detection> retrievePhotoDetections(final Long sequenceId, final Integer sequenceIndex)
+			throws ServiceException {
+		final String url = new HttpQueryBuilder().buildRetrievePhotoDetectionsQuery(sequenceId, sequenceIndex);
+		final Response response = executeGet(url, Response.class);
+		verifyResponseStatus(response);
+		return response.getDetections();
+	}
 
-    public List<Detection> retrievePhotoDetections(final Long sequenceId, final Integer sequenceIndex)
-            throws ServiceException {
-        final String url = new HttpQueryBuilder().buildRetrievePhotoDetectionsQuery(sequenceId, sequenceIndex);
-        final Response response = executeGet(url, Response.class);
-        verifyResponseStatus(response);
-        return response.getDetections();
-    }
+	public Detection retrieveDetection(final Long id) throws ServiceException {
+		final String url = new HttpQueryBuilder().buildRetrieveDetectionQuery(id);
+		final Response response = executeGet(url, Response.class);
+		verifyResponseStatus(response);
+		return response.getDetection();
+	}
 
-    public Detection retrieveDetection(final Long id) throws ServiceException {
-        final String url = new HttpQueryBuilder().buildRetrieveDetectionQuery(id);
-        final Response response = executeGet(url, Response.class);
-        verifyResponseStatus(response);
-        return response.getDetection();
-    }
+	public Cluster retrieveCluster(final Long id) throws ServiceException {
+		final String url = new HttpQueryBuilder().buildRetrieveClusterQuery(id);
+		final Response response = executeGet(url, Response.class);
+		verifyResponseStatus(response);
+		return response.getCluster();
+	}
 
-    public Cluster retrieveCluster(final Long id) throws ServiceException {
-        final String url = new HttpQueryBuilder().buildRetrieveClusterQuery(id);
-        final Response response = executeGet(url, Response.class);
-        verifyResponseStatus(response);
-        return response.getCluster();
-    }
+	public List<Detection> retrieveClusterDetections(final Long id) throws ServiceException {
+		final String url = new HttpQueryBuilder().buildRetrieveClusterDetectionsQuery(id);
+		final Response response = executeGet(url, Response.class);
+		verifyResponseStatus(response);
+		return response.getDetections();
+	}
 
-    public List<Detection> retrieveClusterDetections(final Long id) throws ServiceException {
-        final String url = new HttpQueryBuilder().buildRetrieveClusterDetectionsQuery(id);
-        final Response response = executeGet(url, Response.class);
-        verifyResponseStatus(response);
-        return response.getDetections();
-    }
+	public List<Photo> retrieveClusterPhotos(final Long id) throws ServiceException {
+		final String url = new HttpQueryBuilder().buildRetrieveClusterPhotosQuery(id);
+		final Response response = executeGet(url, Response.class);
+		verifyResponseStatus(response);
+		return response.getPhotos();
+	}
 
-    public List<Photo> retrieveClusterPhotos(final Long id) throws ServiceException {
-        final String url = new HttpQueryBuilder().buildRetrieveClusterPhotosQuery(id);
-        final Response response = executeGet(url, Response.class);
-        verifyResponseStatus(response);
-        return response.getPhotos();
-    }
+	public Photo retrievePhoto(final Long sequenceId, final Integer sequenceIndex) throws ServiceException {
+		final String url = new HttpQueryBuilder().buildRetrievePhotoQuery(sequenceId, sequenceIndex);
+		final Response response = executeGet(url, Response.class);
+		verifyResponseStatus(response);
+		return response.getPhoto();
+	}
 
-    public Photo retrievePhoto(final Long sequenceId, final Integer sequenceIndex) throws ServiceException {
-        final String url = new HttpQueryBuilder().buildRetrievePhotoQuery(sequenceId, sequenceIndex);
-        final Response response = executeGet(url, Response.class);
-        verifyResponseStatus(response);
-        return response.getPhoto();
-    }
+	public List<Sign> listSigns() throws ServiceException {
+		final String url = new HttpQueryBuilder().buildListSignsQuery();
+		final Response response = executeGet(url, Response.class);
+		verifyResponseStatus(response);
+		return response.getSigns();
+	}
 
-    public List<Sign> listSigns() throws ServiceException {
-        final String url = new HttpQueryBuilder().buildListSignsQuery();
-        final Response response = executeGet(url, Response.class);
-        verifyResponseStatus(response);
-        return response.getSigns();
-    }
-
-    public List<String> listRegions() throws ServiceException {
-        final String url = new HttpQueryBuilder().buildListRegionsQuery();
-        final Response response = executeGet(url, Response.class);
-        verifyResponseStatus(response);
-        return response.getRegions();
-    }
+	public List<String> listRegions() throws ServiceException {
+		final String url = new HttpQueryBuilder().buildListRegionsQuery();
+		final Response response = executeGet(url, Response.class);
+		verifyResponseStatus(response);
+		return response.getRegions();
+	}
 }
---
+++ src/org/openstreetmap/josm/plugins/openstreetcam/service/apollo/ClusterConfidenceLevelDeserializer.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2020 Grabtaxi Holdings PTE LTE (GRAB), All rights reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be found in the LICENSE file.
+ *
+ */
+package org.openstreetmap.josm.plugins.openstreetcam.service.apollo;
+
+import java.lang.reflect.Type;
+
+import org.openstreetmap.josm.plugins.openstreetcam.entity.ClusterConfidenceLevel;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+
+/**
+ * @author beata.tautan
+ *
+ */
+class ClusterConfidenceLevelDeserializer implements JsonDeserializer<ClusterConfidenceLevel> {
+
+	private static final String OVERALL_CONFIDENCE = "overallConfidence";
+	private static final String OCR_CONFIDENCW = "ocrConfidence";
+
+	@Override
+	public ClusterConfidenceLevel deserialize(final JsonElement jsonElement, final Type type,
+			final JsonDeserializationContext context) {
+		Double overallConfidence;
+		Double ocrConfidence = null;
+		if (jsonElement instanceof JsonObject) {
+			final JsonObject obj = (JsonObject) jsonElement;
+			overallConfidence = obj.get(OVERALL_CONFIDENCE).getAsDouble();
+			ocrConfidence = obj.get(OCR_CONFIDENCW).getAsDouble();
+		} else {
+			final JsonPrimitive obj = (JsonPrimitive) jsonElement;
+			overallConfidence = obj.getAsDouble();
+		}
+		return overallConfidence != null || ocrConfidence != null
+				? new ClusterConfidenceLevel(overallConfidence, ocrConfidence)
+				: null;
+	}
+}
